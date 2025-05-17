97051b51e0b8870a05e598d6682fa227a76305a3
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-05-18 03:09:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ SILICON_PIC = os.environ.get("SILICON_PIC", "https://envs.sh/lP-.jpg")
 API_ID = int(getenv("API_ID", "29385418"))
 API_HASH = str(getenv("API_HASH", "5737577bcb32ea1aac1ac394b96c4b10"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "-1002222755917") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "deathkingworld") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://towaye6074:A2jR9c7DCr3gX09q@cluster0.g8c3j.mongodb.net/?retryWrites=true&w=majority",))
 DEF_CAP = str(
     getenv(
